a709947513c1659dfb908e7c0f65d074b559d452
Kairat Ubukulov <kairat_ubukulov@mail.ru>
2018-09-12 00:32:46 +0600

    Начался верстка сайта. Надо верстать главную страницу

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,21 +5,64 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Likemoney</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
 
         <!-- Styles -->
         <link rel="stylesheet" href="{{ asset('semantic/semantic.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
         <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 
         <!-- Scripts -->
-        <script src="{{ asset('semantic/jquery-3.3.1.js') }}"></script>
+        <script src="{{ asset('js/jquery-3.3.1.js') }}"></script>
         <script src="{{ asset('semantic/semantic.js') }}"></script>
+        <script src="{{ asset('js/app.js') }}"></script>
         <script src="{{ asset('js/scripts.js') }}"></script>
     </head>
     <body>
+        <div class="wide">
+            <div class="header">
+                <div class="container">
 
+                </div>
+            </div>
+
+            <div class="ui centered grid">
+                <div class="centered row">
+                    <div class="ui fours cards">
+                        <div class="ui card">
+                            <div class="image dimmable">
+                                <div class="ui blurring inverted dimmer transition hidden">
+                                    <div class="content">
+                                        <div class="center">
+                                            <div class="ui teal button">Add Friend</div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <img src="/img/logo.png">
+                            </div>
+                            <div class="content">
+                                <div class="headers">Title</div>
+                                <div class="meta">
+                                    <a class="group">Meta</a>
+                                </div>
+                                <div class="description">One or two sentence description that may go to several lines</div>
+                            </div>
+                            <div class="extra content">
+                                <a class="right floated created">Arbitrary</a>
+                                <a class="friends">
+                                    Arbitrary</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="footer">
+
+            </div>
+        </div>
     </body>
 </html>
