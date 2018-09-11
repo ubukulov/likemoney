44d32edff65663558a3414499ad12d6ff8cdf4c2
Kairat Ubukulov <kairat_ubukulov@mail.ru>
2018-09-11 23:54:29 +0600

    Подготовка и подключение CSS и JS файлы

---
+++ public/js/scripts.js
@@ -0,0 +1,3 @@
+/**
+ * Created by Dauren on 11.09.2018.
+ */
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="{{ asset('semantic/semantic.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+
+        <!-- Scripts -->
+        <script src="{{ asset('semantic/jquery-3.3.1.js') }}"></script>
+        <script src="{{ asset('semantic/semantic.js') }}"></script>
+        <script src="{{ asset('js/scripts.js') }}"></script>
+    </head>
+    <body>
+
+    </body>
+</html>
